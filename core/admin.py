2a29f243d07cfd8dacfa1d3836d69e40dbaf4eb0
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2016-02-17 13:05:54 -0200

    admin on

--- core/admin.py
+++ core/admin.py
@@ -1,2 +1,24 @@
 from django.contrib import admin
+from .models import Viagem, Combustivel, Destino, Origem
 
+
+class OrigemAdmin(admin.ModelAdmin):
+    fields = ['cidade', 'bairro', 'endereco']
+
+
+class DestinoAdmin(admin.ModelAdmin):
+    fields = ['cidade', 'bairro', 'endereco']
+
+
+class CombustivelAdmin(admin.ModelAdmin):
+    fields = ['combustivel', 'preco']
+
+
+class ViagemAdmin(admin.ModelAdmin):
+    fields = ['combustivel', 'origem', 'destino']
+
+
+admin.site.register(Viagem, ViagemAdmin)
+admin.site.register(Combustivel, CombustivelAdmin)
+admin.site.register(Destino, DestinoAdmin)
+admin.site.register(Origem, OrigemAdmin)
--- core/models.py
+++ core/models.py
@@ -9,24 +9,35 @@ COMBUSTIVEL = (
 )
 
 
-class Local(models.Model):
+class Origem(models.Model):
     cidade = models.CharField(max_length=50)
     bairro = models.CharField(max_length=50)
     endereco = models.CharField(max_length=50)
 
 
-class Origem(models.Model):
-    nome = models.CharField(max_length=50)
-
-
 class Destino(models.Model):
-    nome = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=50)
 
 
 class Combustivel(models.Model):
     combustivel = models.CharField(
-        max_length=10,
+        max_length=50,
         choices=COMBUSTIVEL,
         default="g"
     )
     preco = models.FloatField()
+
+
+class Viagem(models.Model):
+    combustivel = models.OneToOneField(
+        Combustivel,
+    )
+    origem = models.OneToOneField(
+        Origem,
+    )
+    destino = models.OneToOneField(
+        Destino,
+        on_delete=models.CASCADE
+    )
