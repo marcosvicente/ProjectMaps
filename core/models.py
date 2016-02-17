871c2fa96f450b15c98819da870cfb25d4adff97
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2016-02-17 09:31:47 -0200

    Models create

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+COMBUSTIVEL = (
+    ('g', 'gasolina')
+    ('a','alcool')
+    ('d', 'diesel')
+)
+
+
+class Local(models.Model):
+    cidade = models.Model()
+    bairro = models.Model()
+    endereco = models.Model()
+
+
+class Origem(models.Model):
+    nome = models.CharField()
+
+
+class Destino(models.Model):
+    nome = models.CharField()
+
+
+class Combustivel(models.Model):
+    combustivel = models.CharField(
+        choices=COMBUSTIVEL, default="g"
+    )
