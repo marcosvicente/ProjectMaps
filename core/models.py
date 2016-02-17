da797c1a0aa3380deeda03fb3c08ea41f0a419de
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2016-02-17 09:39:07 -0200

    Models create

--- core/models.py
+++ core/models.py
@@ -3,27 +3,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 COMBUSTIVEL = (
-    ('g', 'gasolina')
-    ('a','alcool')
-    ('d', 'diesel')
+    ('g', 'gasolina'),
+    ('a', 'alcool'),
+    ('d', 'diesel'),
 )
 
 
 class Local(models.Model):
-    cidade = models.Model()
-    bairro = models.Model()
-    endereco = models.Model()
+    cidade = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=50)
 
 
 class Origem(models.Model):
-    nome = models.CharField()
+    nome = models.CharField(max_length=50)
 
 
 class Destino(models.Model):
-    nome = models.CharField()
+    nome = models.CharField(max_length=50)
 
 
 class Combustivel(models.Model):
     combustivel = models.CharField(
-        choices=COMBUSTIVEL, default="g"
+        max_length=10,
+        choices=COMBUSTIVEL,
+        default="g"
     )
+    preco = models.FloatField()
